f97729ff447ffa7631603eb559c22901c85b9174
Léon Tran-Van <leon.tranvan@hagergroup.com>
2020-11-14 01:00:01 +0100

    basic configuration & colors + breakpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  }
+  
+}
+`
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <GlobalStyle />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
