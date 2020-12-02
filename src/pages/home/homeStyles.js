46cccc795dbab08bfd01eabe76b5ad40ed17e514
Léon Tran-Van <leon.tranvan@hagergroup.com>
2020-12-02 21:56:02 +0100

    review categories mobile

--- src/pages/home/homeStyles.js
+++ src/pages/home/homeStyles.js
@@ -13,15 +13,20 @@ export const Container = styled(motion.div)`
 export const Titleh1 = styled(motion.h1)`
   font-size: 20px;
   text-align: center;
-  margin: 56px 0 40px;
+  margin: 32px 0 24px;
+ 
+  @media (min-width: ${breakpoints.$screenMinXs}) {
+    margin: 56px 0 40px;
+  }
 `
 
 export const CategoryWrapper = styled(motion.div)`
   display: flex;
   flex-direction: column; 
-  margin: 0 16px 56px;
+  margin: 0 16px 40px;
   min-width: 100%;
   @media (min-width: ${breakpoints.$screenMinXs}) {
+  margin: 0 16px 56px;
     flex-direction: row;
     flex-wrap: wrap;
     min-width: 80%;
@@ -31,11 +36,19 @@ export const CategoryWrapper = styled(motion.div)`
 export const Titleh2 = styled(motion.h2)`
   font-size: 18px;
   text-align: center;
-  margin: 0 0 32px;
+  margin: 0 0 24px;
+  @media (min-width: ${breakpoints.$screenMinXs}) {
+    margin: 0 0 32px;
+  }
 `
 export const Form = styled(motion.form)`
   display: flex;
   justify-content: center;
+  width: calc(100% - 32px);
+  
+  @media (min-width: ${breakpoints.$screenMinXs}) {
+      width: unset;
+  }
 `
 
 export const CategorySearchWrapper = styled(motion.div)`
@@ -43,6 +56,8 @@ export const CategorySearchWrapper = styled(motion.div)`
   flex-direction: column; 
   padding: 32px 16px 56px;
   min-width: 100%;
+  width: 100%;
+  
   @media (min-width: ${breakpoints.$screenMinXs}) {
     flex-direction: row;
     flex-wrap: wrap;
